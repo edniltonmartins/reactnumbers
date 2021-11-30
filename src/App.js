08bc0470afea86db117674c48ec213de45caa1d0
EdniltonMartins <ednilton.martins@equiplano.com.br>
2021-11-30 19:57:54 -0300

    Trabalhando com maps

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-function App({title, description}) {
+function App(/**/{title, description}) {
   const [ number, setNumber ] = useState(0);
   return (
     <>
@@ -14,6 +14,27 @@ function App({title, description}) {
         value={number}
         onChange={(/*event*/{target})=> setNumber(parseInt(target.value))}
       ></input>
+
+      {/* Tabuada dando todos os elementos do Array com map */}
+      <ul>
+      {[0,1,2,3,4].map((i)=> (
+        <li>{number} x {i} = {number * i}</li>
+      ))}
+      </ul>
+
+      <h2 className={number >=0 ? "positive" : "negative"}>
+        {number >= 0 ? "Positivo" : "Negativo"}
+      </h2>
+      <ul>
+        {Array.from({ length: 11}).map( (_, i) =>(
+          <li>
+            {number} * {i} = <strong>{number * i}</strong>
+          </li>
+        ))}
+        
+      </ul>  
+       
+      
     </>
   );
 }
